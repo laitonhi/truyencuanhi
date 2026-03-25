5ccdfc3c00244d74369de71cd6fdf71f606a09ab
laitonhi <laitonhi@gmail.com>
2026-03-25 17:09:47 +0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Bản dịch thuộc về Truyện của Nhi',
   },
 }
 
