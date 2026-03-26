798ea73ab11e57f500f78c065ce775d9ef7d2682
laitonhi <laitonhi@gmail.com>
2026-03-26 11:53:31 +0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,24 @@
 export default {
   logo: <span>Truyện của Nhi</span>,
+
   project: {
     link: 'https://github.com',
+  },
+
+  components: {
+    h1: ({ children }) => {
+      return (
+        <h1
+          style={{
+            textAlign: 'center',
+            fontSize: '26px',
+            fontWeight: '700',
+            margin: '30px 0 20px'
+          }}
+        >
+          {children}
+        </h1>
+      )
+    }
   }
 }
